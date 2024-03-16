8844f189345e5c60c997c268a37efcb19e5e6d01
Vinod Tysoe <82728229+Vizzoid@users.noreply.github.com>
2024-03-16 22:07:59 +0000

    added debug logging, functions, and error catching

--- firefox/popup.js
+++ firefox/popup.js
@@ -1,9 +1,21 @@
-browser.tabs.query({currentWindow: true, active: true})
-    .then((tabs) => {
-      let link = tabs[0].url;
+function moveShortsToVideo(tabs) {
+  let link = tabs[0].url;
+  console.log(`Attempting link switch for ${link}`);
 
-      if (/https:\/\/www.youtube.com\/shorts\/*/g.test(link)) {
-        let parts = link.split("shorts/");
-        tabs[0].url = parts[0] + "watch?v=" + parts[1];
-      }
-})
+  if (!/https:\/\/www.youtube.com\/shorts\/*/g.test(link)) {
+    console.log(`Failed, non-YouTube Shorts link`);
+    return;
+  }
+
+  let parts = link.split("shorts/");
+  let videoLink = parts[0] + "watch?v=" + parts[1];
+  console.log(`Success, moving shorts to: ${videoLink}`);
+}
+
+function reportError(error) {
+  console.error(`Error: ${error.message}`);
+}
+
+browser.tabs.query({currentWindow: true, active: true})
+    .then(moveShortsToVideo)
+    .catch(reportError);
